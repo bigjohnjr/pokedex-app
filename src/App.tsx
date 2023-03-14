87defdcc864159d3ffcb930a1fbfcbf3aa47ea7d
John White <jaydub@macbook-pro-5.lan>
2023-03-14 10:06:47 -0500

    captured pokemon addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { useState } from "react";
-import Search from "./components/Search";
 import Pokemon from "./components/Pokemon";
-import CaptureList from "./components/CaptureList";
 import "./App.css";
 
 function App() {
-  // const [searchQuery, setSearchQuery] = React.useState("");
-
   return (
     <div className="App">
-      <div className="left-box">
-        <Search />
-        <Pokemon />
-      </div>
-      <div className="right-box">
+      <Pokemon />
+      {/* <div className="right-box">
         <CaptureList />
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -27,6 +27,7 @@ interface Pokemon {
 }
 
 function Pokemon() {
+  const [capturedPokemon, setCapturedPokemon] = useState<string[]>([]);
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
   const [pokemon, setPokemon] = useState<PokemonType[]>([]);
   const [search, setSearch] = useState<string>("");
@@ -56,7 +57,7 @@ function Pokemon() {
           `https://pokeapi.co/api/v2/pokemon/${search}`
         );
         setSelectedPokemon(response.data);
-        console.log("fetchedSelectedPokemon", response.data);
+        // console.log("fetchedSelectedPokemon", response.data);
       } catch (error) {
         console.log(error);
       }
@@ -87,39 +88,64 @@ function Pokemon() {
     setDropdownClicked(false);
   }
 
+  function handleCapture(e: any) {
+    e.preventDefault();
+    if (selectedPokemon) {
+      const spriteUrl = selectedPokemon?.sprites.front_default;
+      console.log("Selected Pokemon:", selectedPokemon);
+      console.log("Sprite URL:", spriteUrl);
+      setCapturedPokemon((prevCapturedPokemonList) => [
+        ...prevCapturedPokemonList,
+        spriteUrl,
+      ]);
+      console.log("Captured Pokemon:", capturedPokemon);
+    }
+  }
+
   return (
     <>
-      <input
-        type="text"
-        id="search"
-        placeholder="Search"
-        onChange={handleChange}
-        value={search || ""}
-      />
-      <div className="dropdown">{pokeList}</div>
+      <div className="left-box">
+        <input
+          type="text"
+          id="search"
+          placeholder="Search"
+          onChange={handleChange}
+          value={search || ""}
+        />
+        <div className="dropdown">{pokeList}</div>
 
-      {selectedPokemon && (
-        <>
-          <div className="pokemon-type">
-            <img src={selectedPokemon.sprites.front_default} />
-            <div>
-              <h2>
-                {selectedPokemon.name.charAt(0).toUpperCase() +
-                  selectedPokemon.name.slice(1)}
-              </h2>
-              <div className="type">{selectedPokemon.types[0].type.name}</div>
+        {selectedPokemon && (
+          <>
+            <div className="pokemon-type">
+              <img src={selectedPokemon.sprites.front_default} />
+              <div>
+                <h2>
+                  {selectedPokemon.name.charAt(0).toUpperCase() +
+                    selectedPokemon.name.slice(1)}
+                </h2>
+                <div className="type">{selectedPokemon.types[0].type.name}</div>
+              </div>
             </div>
-          </div>
-          <ul className="stats-wrapper">
-            {selectedPokemon.stats.map((stat) => (
-              <li key={stat.stat.name}>
-                {stat.stat.name.toUpperCase()} <span>{stat.base_stat}</span>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-      {dropdownClicked && <button className="capture-btn">Capture</button>}
+            <ul className="stats-wrapper">
+              {selectedPokemon.stats.map((stat) => (
+                <li key={stat.stat.name}>
+                  {stat.stat.name.toUpperCase()} <span>{stat.base_stat}</span>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {dropdownClicked && (
+          <button className="capture-btn" onClick={handleCapture}>
+            Capture
+          </button>
+        )}
+      </div>
+      <div className="right-box">
+        {capturedPokemon.map((spriteUrl) => (
+          <img src={spriteUrl} />
+        ))}
+      </div>
     </>
   );
 }
